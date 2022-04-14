f2856d914baf7c14e98f246f36f6fe624a7a4db2
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-04-14 13:35:27 -0400

    day01

---
+++ src/day01variables/Repeat.java
@@ -0,0 +1,33 @@
+package day01variables;
+
+public class Repeat {
+
+    public static void main(String[] args) {
+
+        int age=12;
+        char initial='A';
+
+        System.out.println(age);
+        System.out.println(initial);
+
+        int a=1,b=2,c=3;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        System.out.println(a+b+c);
+        System.out.println("The sum is "+(a+b+c) );
+
+        int d=4, e=5;
+        System.out.println("The multiplication is "+ d*e);
+
+    }
+
+
+}
+
+
+
+
+
+
---
+++ src/day01variables/Variables01.java
@@ -0,0 +1,40 @@
+package day01variables;
+
+public class Variables01 {
+
+    public static void main(String[] args) {
+        //I am creating integer variable whose name is age
+        int age = 12;
+
+        // i am creating a char variable whose name is initial
+        char initial = 'A';
+
+        //Let's print the values of age and initial variables on the console
+        System.out.println(age);
+
+        System.out.println(initial);
+
+        int weight=65, height=165, distance=450;
+        System.out.println(weight);
+        System.out.println(height);
+        System.out.println(distance);
+
+        System.out.println(weight+height+distance);
+
+        System.out.println("The sum is " + weight + height + distance);
+
+        System.out.println("The sum is " + (weight + height + distance));
+
+        int add= weight + height + distance;
+
+        System.out.println( "The sum is " + add);
+
+        int a01=20, b02=30;
+        System.out.println("the multiplication is " + a01*b02);
+
+
+    }
+
+
+}
+
