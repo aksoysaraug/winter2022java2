d9cd628fa517ae5e2b8c64ba98ad0d61225e9dab
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-04-15 18:06:10 -0400

    day03

---
+++ src/day03typecastingwrapperscanner/TypeCasting01.java
@@ -0,0 +1,21 @@
+package day03typecastingwrapperscanner;
+
+public class TypeCasting01 {
+
+    public static void main(String[] args) {
+
+        byte b=47;
+        int i=b;
+        System.out.println(i);
+
+
+    short s=257;
+    byte y=(byte)s;
+
+        System.out.println(y);
+    }
+
+
+
+
+}
