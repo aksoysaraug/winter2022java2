d171b1f837f05a5e0aa3f084a9962a7457ad5ba4
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-04-15 18:06:40 -0400

    Wrapper

---
+++ src/day03typecastingwrapperscanner/WrapperClasses01.java
@@ -0,0 +1,31 @@
+package day03typecastingwrapperscanner;
+
+public class WrapperClasses01 {
+    public static void main(String[] args) {
+
+        char c='d';
+        Character wc=c;
+        System.out.println(wc);
+
+        int i=12;
+        Integer wi=i;
+        System.out.println(wi);
+
+
+        Short s=23;
+        short ps=s;
+        System.out.println(ps);
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(Short.MAX_VALUE+Long.MIN_VALUE);
+
+
+
+
+    }
+}
