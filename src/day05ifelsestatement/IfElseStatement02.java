7ce9ca80a974a4c8493026f6326a509e865d4a13
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-04-27 16:38:56 -0400

    If-Else

---
+++ src/day05ifelsestatement/IfElseStatement02.java
@@ -0,0 +1,23 @@
+package day05ifelsestatement;
+
+public class IfElseStatement02 {
+    public static void main(String[] args) {
+
+        //If we have daytime class; I can join it
+        //If we have night time class, I can join it
+
+        boolean day=false;
+        boolean night=false;
+
+        if ( day||night){
+            System.out.println("I can join the class");
+        } else {
+            System.out.println("I CANNOT join the class");
+        }
+
+
+
+
+    }
+
+}
