37d3d81d5c1a1770430fa2a2abddd9574687acc4
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-05-09 13:06:42 -0400

    Do while loop

---
+++ src/day11dowhileconstructors/DoWhile01.java
@@ -0,0 +1,24 @@
+package day11dowhileconstructors;
+
+public class DoWhile01 {
+
+    public static void main(String[] args) {
+
+//        What is the difference between while loop and do-while loop?
+//        In while loop; the codes inside the loop body are not ex
+        int i=1;
+        while(i<1){
+            System.out.println("Hello!");
+            i++;
+        }
+
+//      do-while
+        int k=1;
+        do{
+            System.out.println("Hello!");
+            k++;
+        }while(k<1);
+
+
+    }
+}
