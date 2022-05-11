a2603b631cd8423497a97fbd46164feac9fff818
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-05-11 00:34:00 -0400

    Question

---
+++ src/day11dowhileconstructors/Constructors01.java
@@ -0,0 +1,10 @@
+package day11dowhileconstructors;
+
+public class Constructors01 {
+    public static void main(String[] args) {
+
+
+        Constructors01 onj01 = new Constructors01();
+
+    }
+}
