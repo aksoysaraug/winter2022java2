53ab5903a826e382532a82b1a153b6bd090c8de5
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-05-13 22:47:06 -0400

    Question

---
+++ src/day11practise02/Q06.java
@@ -0,0 +1,22 @@
+package day11practise02;
+
+public class Q06 {
+    public static void main(String[] args) {
+
+        /*
+ Print on the console numbers from 1 to 100.
+ But do not use any number in your code!
+ */
+
+        int n='a'/'a'; //==>1    you can also use 'b'-'a'
+        int m='d'; // ==>ASCII value of d is 100
+
+        for (int i=n; i<=m; i++){
+            System.out.print(i+" ");
+        }
+
+
+
+
+    }
+}
