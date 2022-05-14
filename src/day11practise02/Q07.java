45c1f95d20eae3367a9c518f6ec95bed81b52928
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-05-13 23:01:20 -0400

    Question

---
+++ src/day11practise02/Q07.java
@@ -0,0 +1,30 @@
+package day11practise02;
+
+import java.util.Scanner;
+
+public class Q07 {
+    public static void main(String[] args) {
+
+        /*
+ Print numbers from given number till the next multiple of 10. e.g.,
+ if input is 5 it should print 5, 6, 7, 8, 9, 10 or
+ if input is 27 it should print 27, 28, 29, 30 or
+ if input is 40 then it should print 40.
+ Use while loop..
+*/
+        Scanner scan=new Scanner(System.in);
+        System.out.println("Enter a number");
+        
+        int i=scan.nextInt();
+
+        while(i%10!=0){
+            System.out.print(i +" ");
+
+            i++;
+        }
+        System.out.print(i);
+
+
+    }
+
+}
