1039627e9258231b079b1b0723edb4d20aa0fb5d
Ummu Gulsum Aksoy <eczglsm89@gmail.com>
2022-05-23 17:51:42 -0400

    Arrays

---
+++ src/day14arraysforeachloop/Arrays03.java
@@ -0,0 +1,27 @@
+package day14arraysforeachloop;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Arrays03 {
+    public static void main(String[] args) {
+
+//        Create an Array by getting array elements from user
+
+        Scanner scan=new Scanner(System.in);
+        System.out.println("How many elements will you store inside the array?");
+        int len= scan.nextInt();
+
+        String sArray[]= new String[len];
+
+        for(int i=0; i<len; i++){
+
+            System.out.println("Enter a String");
+            sArray[i]=scan.next();
+        }
+        System.out.println(Arrays.toString(sArray));
+
+
+
+    }
+}
