1056bf72832c226795c61cc75fb6a6bf9cc358ac
Sara Aksoy <eczglsm89@gmail.com>
2022-06-12 07:40:53 -0400

    Interview Question

---
+++ src/day20practiseinterview/Interview01.java
@@ -0,0 +1,24 @@
+package day20practiseinterview;
+
+public class Interview01 {
+    public static void main(String[] args) {
+        /*
+      Type a program to print the integers from 1 to 100 without using any integer
+   */
+//        1. Way
+        int a='a'/'a';
+        int b='d';
+
+        for (int i=a;i<'e';i++){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+
+//        2. Way
+        for (char c='c'/'c'; c<'e'; c++){
+            System.out.print((int)c+" ");
+        }
+
+
+    }
+}
