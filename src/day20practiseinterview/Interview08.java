125d9b59ab962119d5e0bcc2cd2ca21ba195c8f3
Sara Aksoy <eczglsm89@gmail.com>
2022-06-12 07:58:36 -0400

    Interview Question

---
+++ src/day20practiseinterview/Interview08.java
@@ -0,0 +1,24 @@
+package day20practiseinterview;
+
+public class Interview08 {
+    public static void main(String[] args) {
+
+//        What is the output
+                String str = "M ";
+
+                str = str.concat("E ");
+
+                String  add  = "S ";
+
+                str = str.concat(add);
+
+                str.replace("S" , "T");
+
+                str = str.concat(add);
+
+                System.out.println(str);
+
+
+
+    }
+}
