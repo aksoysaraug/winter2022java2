48d911e074f583382885c3574a6a50232611617c
Sara Aksoy <eczglsm89@gmail.com>
2022-06-12 07:56:16 -0400

    Interview Question

---
+++ src/day20practiseinterview/Interview07.java
@@ -0,0 +1,28 @@
+package day20practiseinterview;
+
+public class Interview07 {
+    public static void main(String[] args) {
+
+
+                int num = 10;
+                do {
+                    System.out.print(num-- + " ");
+
+                } while (--num == 0);
+
+                System.out.println(num);
+
+    /*
+      What is the result?
+      A) 543210
+      B) 9
+      C) 421
+      D) 10 8
+      E) Nothing is printed
+   */
+
+
+
+
+    }
+}
