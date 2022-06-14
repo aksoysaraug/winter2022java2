ac97c5574f8547befaa7d2b4fe6992ae1f3b201f
Sara Aksoy <eczglsm89@gmail.com>
2022-06-13 20:47:40 -0400

    Student

---
+++ src/day21varargsstringbuildersaccessmodifiers/Student.java
@@ -0,0 +1,12 @@
+package day21varargsstringbuildersaccessmodifiers;
+
+public class Student extends Parent{
+
+    public String name="Ali Can";
+    Integer age=12;
+    private char initial='A';
+
+
+
+
+}
