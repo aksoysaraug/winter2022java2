c2cc9020a1cd78c5fa241fdbd217b64490fd63b4
Sara Aksoy <eczglsm89@gmail.com>
2022-06-13 20:52:13 -0400

    Student Runner

---
+++ src/day21varargsstringbuildersaccessmodifiers/StudentRunner.java
@@ -0,0 +1,14 @@
+package day21varargsstringbuildersaccessmodifiers;
+
+public class StudentRunner {
+    public static void main(String[] args) {
+
+    Student st1=new Student();
+        st1.name.toLowerCase();
+        System.out.println(st1.age);
+        st1.illness.length();
+
+        Parent p1=new Parent();
+
+    }
+}
