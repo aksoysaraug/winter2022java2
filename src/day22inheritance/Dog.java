fbb1e3f1151343d41adff5c582c52c10014a3bf4
Sara Aksoy <eczglsm89@gmail.com>
2022-06-17 17:23:08 -0400

    Inheritance

---
+++ src/day22inheritance/Dog.java
@@ -0,0 +1,13 @@
+package day22inheritance;
+
+public class Dog extends Mammals{
+
+
+    int a=0;
+    public void bark(){
+        System.out.println("Dogs bark...");
+    }
+    public Dog(){
+        System.out.println("Dog constructor");
+    }
+}
