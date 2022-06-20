adb42be1160bb327ab45e54552c16dff458daad8
Sara Aksoy <eczglsm89@gmail.com>
2022-06-20 11:01:19 -0400

    Inheritance

---
+++ src/day23inheritance/Car.java
@@ -0,0 +1,15 @@
+package day23inheritance;
+
+public class Car extends Motorized{
+
+    public Car (String make){
+        super(16); //This is the constructor of parent
+        System.out.println("Car constructor with String parameter");
+    }
+
+    public Car (int price){
+        super(20);
+        System.out.println("Car constructor with integer parameter");
+    }
+
+}
