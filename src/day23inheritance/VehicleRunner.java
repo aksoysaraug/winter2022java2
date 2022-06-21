ca7917ec8fb2cd612d1606de56a3a41b60f87ab6
Sara Aksoy <eczglsm89@gmail.com>
2022-06-21 14:38:58 -0400

    Inheritance

---
+++ src/day23inheritance/VehicleRunner.java
@@ -0,0 +1,19 @@
+package day23inheritance;
+
+public class VehicleRunner {
+    public static void main(String[] args) {
+
+        Car car1=new Car("Honda");
+
+        System.out.println("=======");
+
+        Bus bus1=new Bus(25);
+
+        System.out.println("=======");
+
+        Bicycle bicycle=new Bicycle("Nike");
+
+
+
+    }
+}
